a934d5711d20ed9005c891a2d07fe18e4b429550
GudlaSupriya <supriyagudla@gmail.com>
2023-10-20 09:46:19 +0530

    Repository commit

---
+++ src/main/java/com/cg/repository/MerchantRepository.java
@@ -0,0 +1,9 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.entity.Merchant;
+
+public interface MerchantRepository extends JpaRepository<Merchant,Integer>{
+
+}
