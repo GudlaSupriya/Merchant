5f19cc9d8e65e440aa9f7b768238bb7821cb1e3b
GudlaSupriya <supriyagudla@gmail.com>
2023-10-20 10:42:19 +0530

    Repository commit

--- src/main/java/com/cg/repository/MerchantRepository.java
+++ src/main/java/com/cg/repository/MerchantRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cg.entity.Merchant;
 
 public interface MerchantRepository extends JpaRepository<Merchant,Integer>{
-public Merchant findbyproductName(String productName);
+
 
 	
 
