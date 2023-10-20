f5700f837b1130d32040a42bc6afd2ff0ac606a6
GudlaSupriya <supriyagudla@gmail.com>
2023-10-20 09:41:35 +0530

    Service commit

---
+++ src/main/java/com/cg/service/MerchantService.java
@@ -0,0 +1,46 @@
+package com.cg.service;
+
+import org.springframework.stereotype.Service;
+
+import com.cg.dao.MerchantDao;
+import com.cg.entity.Merchant;
+import java.util.*;
+
+
+@Service
+
+
+
+public class MerchantService implements MerchantDao {
+
+	
+
+	List<Merchant> merchants = new ArrayList<Merchant>();
+
+ 
+
+
+
+	public String addProduct(Merchant merchant) {
+
+		merchants.add(merchant);
+
+		return "Merchant added Successfully";
+
+	}
+
+
+
+	@Override
+	public List<Merchant> getMerchantList() {
+		
+		return merchants;
+	}
+
+ 
+
+	
+
+ 
+
+}
